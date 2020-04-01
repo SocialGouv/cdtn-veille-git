8e5f4b8ab7e18d902c3f24058d05e984428ce5f8
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-04-01 16:20:15 +0200

    feat: use fiches-vdd/data/index

--- packages/frontend/src/repos.js
+++ packages/frontend/src/repos.js
@@ -30,12 +30,12 @@ const getTreeDiffKali = (path, hash) =>
     cloneDir: `${GIT_STORAGE}/socialgouv/kali-data`,
     compareFn: compareKaliArticles,
     path,
-    hash
+    hash,
   }).then(
     ({ tree2, changes }) =>
       tree2 && {
         ...tree2.data,
-        changes
+        changes,
       }
   );
 
@@ -44,75 +44,77 @@ const getTreeDiffLegi = (path, hash) =>
     cloneDir: `${GIT_STORAGE}/socialgouv/legi-data`,
     compareFn: compareLegiArticles,
     path,
-    hash
+    hash,
   }).then(
     ({ tree2, changes }) =>
       tree2 && {
         ...tree2.data,
-        changes
+        changes,
       }
   );
 
-const legiPattern = /^data\/LEGITEXT000006072050\.json$/;
+const legiPattern = /^data\/LEGITEXT(000006072050|000006073189)\.json$/;
 const kaliPattern = /^data\/KALI(?:CONT|ARTI)\d+\.json$/;
 const fichesVddPattern = /^data\/[^/]+\/.*.json$/;
 
 // add file change details to some commit
-const commitProcessor = ({ source, filterPath, getFileDiff }) => async commit =>
+const commitProcessor = ({ source, filterPath, getFileDiff }) => async (
+  commit
+) =>
   await {
     source,
     ...commit,
     files: await serialExec(
-      commit.files.filter(filterPath).map(file => async () => ({
+      commit.files.filter(filterPath).map((file) => async () => ({
         path: file.path,
-        ...(await getFileDiff(file.path, commit.hash))
+        ...(await getFileDiff(file.path, commit.hash)),
       }))
-    )
+    ),
   };
 
 const legiCommitProcessor = commitProcessor({
   source: "LEGI",
-  filterPath: file => file.path.match(legiPattern),
-  getFileDiff: getTreeDiffLegi
+  filterPath: (file) => file.path.match(legiPattern),
+  getFileDiff: getTreeDiffLegi,
 });
 
 const kaliCommitProcessor = commitProcessor({
   source: "KALI",
-  filterPath: file => file.path.match(kaliPattern),
-  getFileDiff: getTreeDiffKali
+  filterPath: (file) => file.path.match(kaliPattern),
+  getFileDiff: getTreeDiffKali,
 });
 
-const ficheSpCommitProcessor = async commit => {
+const ficheSpCommitProcessor = async (commit) => {
   const filesChanged = await getFilesChanged({
     cloneDir: `${GIT_STORAGE}/socialgouv/fiches-vdd`,
-    hash: commit.hash
+    hash: commit.hash,
   });
 
   const changes = {
     added: filesChanged.added.map(addVddData),
     removed: filesChanged.removed.map(addVddData),
-    modified: filesChanged.modified.map(addVddData)
+    modified: filesChanged.modified.map(addVddData),
   };
 
   return {
     source: "FICHES-SP",
     ...commit,
-    changes
+    changes,
   };
 };
 
 // commit details never change, lets memoize them by hash
-const memoizeProcessor = processor =>
+const memoizeProcessor = (processor) =>
   memoizee(processor, {
-    normalizer: commit => commit[0].hash,
-    promise: true
+    normalizer: (commit) => commit[0].hash,
+    promise: true,
   });
 
 const getFicheMeta = (fiche, name) =>
   fiche &&
   fiche.children &&
   fiche.children.length &&
-  fiche.children[0].children.find(c => c.name === name);
+  fiche.children[0].children.find((c) => c.name === name);
 
 const getFicheMetaText = (fiche, name) => {
   const node = getFicheMeta(fiche, name);
@@ -126,32 +128,33 @@ const getFicheMetaText = (fiche, name) => {
   );
 };
 
-const getFicheTitle = data => getFicheMetaText(data, "dc:title");
-const getFicheSubject = data => getFicheMetaText(data, "dc:subject");
-const getFicheAriane = data => {
+const getFicheTitle = (data) => getFicheMetaText(data, "dc:title");
+const getFicheSubject = (data) => getFicheMetaText(data, "dc:subject");
+const getFicheAriane = (data) => {
   const fil = getFicheMeta(data, "FilDAriane");
   return (
     (fil &&
       fil.children &&
       fil.children.length &&
-      fil.children.map(c => c.children[0].text).join(" > ")) ||
+      fil.children.map((c) => c.children[0].text).join(" > ")) ||
     null
   );
 };
+const fichesVDD = require(`@socialgouv/fiches-vdd/data/index.json`);
 
-const addVddData = path => {
-  // this slow down build considerably
+const addVddData = (path) => {
   try {
-    const fiche = require(`@socialgouv/fiches-vdd/${path}`);
-    //const fiche = { id: "test", title: "pouet", subject: "kikoo", theme: "lol" };
+    const [type, id] = path
+      .replace(/^data\/([^/]+\/[^.]+)\.json$/, "$1")
+      .split("/");
+
+    const fiche = fichesVDD.find((f) => f.type === type && f.id === id);
+    if (!fiche) {
+      console.log("type, id", type, id, path);
+    }
     return {
       path,
-      data: {
-        id: fiche.id || path || null,
-        title: getFicheTitle(fiche),
-        subject: getFicheSubject(fiche),
-        theme: getFicheAriane(fiche)
-      }
+      data: fiche,
     };
   } catch (e) {
     return { path, data: {} };
@@ -162,21 +165,21 @@ const repos = {
   "socialgouv/legi-data": {
     url: `https://github.com/socialgouv/legi-data.git`,
     cloneDir: `${GIT_STORAGE}/socialgouv/legi-data`,
-    filterPath: path => path.match(legiPattern),
-    processCommit: legiCommitProcessor
+    filterPath: (path) => path.match(legiPattern),
+    processCommit: legiCommitProcessor,
   },
   "socialgouv/kali-data": {
     url: `https://github.com/socialgouv/kali-data.git`,
     cloneDir: `${GIT_STORAGE}/socialgouv/kali-data`,
-    filterPath: path => path.match(kaliPattern),
-    processCommit: kaliCommitProcessor
+    filterPath: (path) => path.match(kaliPattern),
+    processCommit: kaliCommitProcessor,
   },
   "socialgouv/fiches-vdd": {
     url: `https://github.com/socialgouv/fiches-vdd.git`,
     cloneDir: `${GIT_STORAGE}/socialgouv/fiches-vdd`,
-    filterPath: path => path.match(fichesVddPattern),
-    processCommit: ficheSpCommitProcessor
-  }
+    filterPath: (path) => path.match(fichesVddPattern),
+    processCommit: ficheSpCommitProcessor,
+  },
 };
 
 export default repos;
